86fb43b2ed37ff29c1a32d6852c89a7df8224d3a
CapoRay0 <caporay0.0@gamil.com>
2021-08-12 15:01:49 +0800

    【Web API】泛型處理常式 ASHX

---
+++ WebForm_AccountingNote/AccountingNote/Handlers/AccountingNoteHandler.ashx
@@ -0,0 +1,1 @@
+﻿<%@ WebHandler Language="C#" CodeBehind="AccountingNoteHandler.ashx.cs" Class="Ray0728am.Handlers.AccountingNoteHandler" %>
---
+++ WebForm_AccountingNote/AccountingNote/Handlers/AccountingNoteHandler.ashx.cs
@@ -0,0 +1,87 @@
+﻿using AccountingNote.DBsource;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ray0728am.Handlers
+{
+    /// <summary>
+    /// AccountingNoteHandler 的摘要描述
+    /// </summary>
+    public class AccountingNoteHandler : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            string actionName = context.Request.QueryString["ActionName"];
+
+            if (string.IsNullOrEmpty(actionName))
+            {
+                context.Response.StatusCode = 400;
+                context.Response.ContentType = "text/plain";
+                context.Response.Write("ActionName is required");
+                context.Response.End();
+            }
+            
+            if(actionName == "create")
+            {
+                string caption = context.Request.Form["Caption"];
+                string amountText = context.Request.Form["Amount"];
+                string actTypeText = context.Request.Form["ActType"];
+                string body = context.Request.Form["Body"];
+
+                // ID of Ray
+                string id = "7E85BB22-C671-4150-8B97-1A6756ACFD72";
+
+                // 必填檢查
+                if (string.IsNullOrWhiteSpace(caption) ||
+                    string.IsNullOrWhiteSpace(amountText) ||
+                    string.IsNullOrWhiteSpace(actTypeText))
+                {
+                    this.ProcessError(context, "caption, amount, actType is required.");
+
+                    return;
+                }
+
+                // 轉型
+                int tempAmount, tempActType;
+                if (!int.TryParse(amountText, out tempAmount) ||
+                    !int.TryParse(actTypeText, out tempActType))
+                {
+                    this.ProcessError(context, "amount, actType should be a integer.");
+
+                    return;
+                }
+
+                //建立流水帳
+                AccountingManager.CreateAccounting(id, caption, tempAmount, tempActType, body);
+
+                context.Response.ContentType = "text/plain";
+                context.Response.Write("ok");
+            }
+            else if (actionName == "update")
+            {
+
+            }else if(actionName == "delete")
+            {
+
+            }
+
+        }
+        private void ProcessError(HttpContext context, string msg)
+        {
+            context.Response.StatusCode = 400;
+            context.Response.ContentType = "text/plain";
+            context.Response.Write(msg);
+            context.Response.End();
+        }
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
